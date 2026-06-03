4af67fc10cf6d8436727c5037c5f514504c5c6a1
Khanylle19 <roger.khanylle19@gmail.com>
2026-06-04 01:13:13 +0800

    fix: update api/index.php path to myApp/public

--- api/index.php
+++ api/index.php
@@ -1,4 +1,4 @@
 <?php
 
-// Forward Vercel requests to the true root public entry point
-require dirname(__DIR__) . '/public/index.php';
+// Forward Vercel requests to the Laravel public entry point
+require dirname(__DIR__) . '/myApp/public/index.php';
