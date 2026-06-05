696c9e6ff01043bb392882745c0a379149915439
Khanylle19 <roger.khanylle19@gmail.com>
2026-06-05 16:12:46 +0800

    Fix toast DOMContentLoaded issue

--- myApp/resources/views/layouts/app.blade.php
+++ myApp/resources/views/layouts/app.blade.php
@@ -128,30 +128,26 @@
 
 @if(session('success'))
 <script>
-document.addEventListener('DOMContentLoaded', function() {
-    Swal.fire({
-        toast: true,
-        position: 'top-end',
-        icon: 'success',
-        title: "{{ session('success') }}",
-        showConfirmButton: false,
-        timer: 3000
-    });
+Swal.fire({
+    toast: true,
+    position: 'top-end',
+    icon: 'success',
+    title: "{{ session('success') }}",
+    showConfirmButton: false,
+    timer: 3000
 });
 </script>
 @endif
 
 @if(session('error'))
 <script>
-document.addEventListener('DOMContentLoaded', function() {
-    Swal.fire({
-        toast: true,
-        position: 'top-end',
-        icon: 'error',
-        title: "{{ session('error') }}",
-        showConfirmButton: false,
-        timer: 3000
-    });
+Swal.fire({
+    toast: true,
+    position: 'top-end',
+    icon: 'error',
+    title: "{{ session('error') }}",
+    showConfirmButton: false,
+    timer: 3000
 });
 </script>
 @endif
